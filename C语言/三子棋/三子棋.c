ddec671a2ae44e549068e5ac358550690054e32d
wuxin20 <1049233037@qq.com>
2019-05-06 22:53:03 +0800

    提交代码

---
+++ C语言/三子棋/三子棋.c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//1.完成课堂中的三子棋程序。
+
+#include "game.h"
+
+void menu()
+{
+	printf("*************************\n");
+	printf("***  1.play   0.exit  ***\n");
+	printf("*************************\n");
+}
+
+void game()
+{
+	char board[ROW][COL] = { 0 };
+	char ret = 0;
+	InitBoard(board, ROW, COL);
+	DisplayBoard(board, ROW, COL);
+	while (1)
+	{
+		ComputerMove(board, ROW, COL);
+		ret = IsWin(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);	
+		PlayerMove(board, ROW, COL);
+		ret = IsWin(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		
+	}
+	if (ret == '*')
+	{
+		printf("玩家赢\n");
+	}
+	else if (ret == '#')
+	{
+		printf("电脑赢\n");
+	}
+	else if (ret == 'Q')
+	{
+		printf("平局\n");
+	}
+	DisplayBoard(board, ROW, COL);
+}
+
+int main()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do {
+		menu();
+		printf ("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误，请重新选择！\n");
+			break;
+		}
+
+	} while (input);
+	system("pause");
+	return 0;
+}
