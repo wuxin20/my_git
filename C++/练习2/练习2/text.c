51b06557c99e1a3f153d5456d58d4b0eb2b78e23
wuxin20 <1049233037@qq.com>
2019-06-09 22:33:03 +0800

    提交代码

---
+++ C++/练习2/练习2/text.c
@@ -0,0 +1,37 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		printf(" %d\n", i);
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int a = 2;
+	int b = 3;
+	if (a == b)
+	{
+		printf("keke\n");
+	}
+	else if (a>b)
+	{
+		printf("haha\n");
+	}
+	else
+	{
+		printf("hehe\n");
+	}
+	system("pause");
+	return 0;
+}
+
