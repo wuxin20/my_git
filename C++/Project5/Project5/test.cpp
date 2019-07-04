7784a7a429b993ee27c8d80eb24b7677a6ee3f73
wuxin20 <1049233037@qq.com>
2019-07-04 21:26:46 +0800

    提交代码

---
+++ C++/Project5/Project5/test.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+//¼üÖµ¶Ô£º
+template <class T1,class T2>
+struct pair
+{
+
+	typedef T1 first_type;
+	typedeff T2 second_type;
+
+	T1 first;
+	T2 sceond;
+	pair()
+		:first(T1())
+		, second(T2)
+	{}
+	
+	pair(const T1& a, const T2& b)
+		:first(a)
+		, second(b)
+	{}
+
+
+};
+*/
